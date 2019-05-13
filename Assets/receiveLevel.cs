ee2eced1a075ecdf738defbb70aefcdba5a33dc3
Eric Pinzur <ericpinzur@otherinbox.com>
2019-05-12 19:50:48 -0500

    set and receive sliders

---
+++ Assets/receiveLevel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.Threading;
+using System.Net.WebSockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class receiveLevel : MonoBehaviour
+{
+    public Slider slider;
+
+    bool running = false;
+
+    async void Update()
+    {
+        if (!running) {
+            running = true;
+            try
+            {
+                using (ClientWebSocket ws = new ClientWebSocket())
+                {
+                    Debug.Log("created socket");
+                    Uri serverUri = new Uri("ws://localhost:3000/");
+                    await ws.ConnectAsync(serverUri, CancellationToken.None);
+                    Debug.Log("Connected?");
+                    while (ws.State == WebSocketState.Open)
+                    {
+                        /*
+                        Console.Write("Input message ('exit' to exit): ");
+                        string msg = Console.ReadLine();
+                        if (msg == "exit")
+                        {
+                            break;
+                        }
+                        ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
+                        await ws.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
+                        */
+                        ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
+                        WebSocketReceiveResult result = await ws.ReceiveAsync(bytesReceived, CancellationToken.None);
+                        String value = Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count);
+                        //Debug.Log(value);
+                        slider.value = float.Parse(value);
+                    }
+                }
+            } catch (WebSocketException exp)
+            {
+                Debug.Log("Caught exception: " + exp.Message);
+                running = false;
+            }
+        }
+    }
+}
---
+++ Assets/setlevel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class setlevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+
+    public void SetValue(float value)
+    {
+        /*WWWForm form = new WWWForm();
+        form.AddField("level", value.ToString());
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:3000/test", form))
+        {
+            www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }*/
+
+        try
+        {
+            HttpWebRequest request = WebRequest.CreateHttp("http://localhost:3000/test");
+
+            var postData = "level=" + value.ToString();
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+        catch (WebException exp)
+        {
+            Debug.Log("Unabled to connect to server: " + exp.Message);
+        }
+
+        //Debug.Log("Response: " + responseString);
+    }
+}
