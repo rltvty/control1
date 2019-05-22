b1a50be1f059bcb2b7e0097b5c49e43916e13c8b
Eric Pinzur <ericpinzur@otherinbox.com>
2019-05-21 23:39:30 -0500

    experimented with a level meter control

---
+++ Assets/GameHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField] public LevelMeter levelMeter;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        float level = 1f;
+        float change = -0.01f;
+        FunctionPeriodic.Create(() =>
+        {
+
+            level += change;
+            if (level <= 0f)
+            {
+                level = 0f;
+                change = change * -1;
+            }
+            if (level >= 1f)
+            {
+                level = 1f;
+                change = change * -1;
+            }
+            levelMeter.SetLevel(level);
+        }, .03f);
+    }
+
+   
+}
---
+++ Assets/LevelMeter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMeter : MonoBehaviour
+{
+    private Transform bar;
+    private SpriteRenderer barSprite;
+    private Vector3 level;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        bar = transform.Find("Bar");
+        barSprite = bar.Find("BarSprite").GetComponent<SpriteRenderer>();
+        level = new Vector3(0f, 1f);
+
+    }
+
+    public void SetLevel(float normalizedLevel)
+    {
+        level.x = normalizedLevel;
+        bar.localScale = level;
+        if (normalizedLevel < 0.5f)
+        {
+            barSprite.color = Color.green;
+        } 
+        else if (normalizedLevel < 0.9f)
+        {
+            barSprite.color = Color.cyan;
+        }
+        else
+        {
+            barSprite.color = Color.red;
+        }
+    }
+}
